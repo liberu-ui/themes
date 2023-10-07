5c20299ff586fba3c6fb140fd26744b9b665629c
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-07 20:34:48 +0000

    Replace JavaScript with TypeScript

--- src/bulma/register.js
+++
@@ -1,4 +0,0 @@
-import App from '@liberu-ui/ui/src/core/app';
-import ThemeSelector from './components/settings/ThemeSelector.vue';
-
-App.registerSettingsItem('theme-selector', ThemeSelector, 200);
